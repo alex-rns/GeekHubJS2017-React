4a497384ba8e7f8ecea264b4cfa905ade28a7b79
alex-rns <alexrns3@gmail.com>
2018-01-12 23:41:42 +0200

    react lesson

--- public/js/chat/controller/messages.js
+++ public/js/chat/controller/messages.js
@@ -6,10 +6,47 @@ function messagesController() {
         }
     });
 
-    var form = newMessageFormView('#new-message-form', {
+    let form = newMessageFormView('#new-message-form', {
         onSend: function (text) {
             messages.add({text: text});
             form.clear();
         }
     });
+
+    reactMessagesView(messages, {});
+    reactMessagesView(messages2, {});
+
 }
+
+function reactMessagesView(messages) {
+
+    let items = messages.map(function (message) {
+        return (
+            <li class="message">
+                <div>{message.text}</div>
+                <button type="button">Delete</button>
+            </li>
+        )
+    });
+
+    return <ul class="messages">{items}</ul>;
+};
+
+function main(applicationState) {
+    let root;
+
+    if (applicationState.page === 'chat'){
+        root = (
+            <chat>
+                {reactMessagesView(applicationState.messages)}
+            </chat>
+        );
+    }
+    else if (applicationState.page === 'homeworks'){
+        root = [
+            <homework id="1"/>,
+            <homework id="2"/>
+        ];
+    }
+}
+
